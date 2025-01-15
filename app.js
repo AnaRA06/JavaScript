40dac95247dc12d9a6edd7be935a825e27e8050b
AnaRA06 <aramos48@ucol.mx>
2025-01-15 16:48:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
+
+
+alert('¡Bienvenido y bienvenida a nuestro sitio web!');
+
+let nombre = "Lua" = prompt('Ingresa tu nombre:');
+let edad = 25 = prompt('Ingresa tu edad');
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert('¡Error! Completa todos los campos');
+
+let mensajeDeError =  '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir');
+}
